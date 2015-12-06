249c8bf460b120447a319fdb6d3bcfc905c1c649
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2015-12-06 21:57:30 +0800

    Refactor tableize

--- test/lib/index_test.js
+++ test/lib/index_test.js
@@ -3,6 +3,7 @@ var hl = require('../../index')
 describe('my project', function () {
   it('works', function () {
     return hl(['bal'])
+      .then(hl.tableize)
       .then((data) => {
         console.log('return', JSON.stringify(data, null, 2))
       })
