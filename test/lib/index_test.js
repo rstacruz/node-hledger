1636263fa793f30a5119ef6e3aec891035795ea1
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2015-12-06 22:17:09 +0800

    Handle errors

--- test/lib/index_test.js
+++ test/lib/index_test.js
@@ -1,21 +1,40 @@
 var hl = require('../../index')
 
-describe('my project', function () {
-  before(function () {
-    return hl(['-f', 'test/fixtures/test.ledger', 'bal'])
-      .then(hl.tableize)
-      .then((data) => {
-        this.data = data
+describe('hledger', function () {
+  describe('basic test', function () {
+    before(function () {
+      return hl(['-f', 'test/fixtures/test.ledger', 'bal'])
+        .then(hl.tableize)
+        .then((data) => {
+          this.data = data
+        })
+    })
+
+    it('parses 1st account', function () {
+      expect(this.data[0])
+        .toEqual({ account: 'Assets', balance: '$250' })
+    })
+
+    it('parses 2nd account', function () {
+      expect(this.data[1])
+        .toEqual({ account: 'Assets:Checking', balance: '$150' })
+    })
+  })
+
+  describe('errors', function () {
+    before(function () {
+      process.env.HLEDGER_BIN = 'nonexistentexecutable'
+    })
+
+    after(function () {
+      delete process.env.HLEDGER_BIN
+    })
+
+    it('are handled', function () {
+      return hl(['...'])
+      .catch((err) => {
+        expect(err.message).toEqual('spawn nonexistentexecutable ENOENT')
       })
-  })
-
-  it('parses 1st account', function () {
-    expect(this.data[0])
-      .toEqual({ account: 'Assets', balance: '$250' })
-  })
-
-  it('parses 2nd account', function () {
-    expect(this.data[1])
-      .toEqual({ account: 'Assets:Checking', balance: '$150' })
+    })
   })
 })
