c4df6d838d400ef83dea7eab24ce40a0c9531a09
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2015-12-06 22:07:40 +0800

    Make tests more deterministic

--- test/lib/index_test.js
+++ test/lib/index_test.js
@@ -1,11 +1,21 @@
 var hl = require('../../index')
 
 describe('my project', function () {
-  it('works', function () {
-    return hl(['bal'])
+  before(function () {
+    return hl(['-f', 'test/fixtures/test.ledger', 'bal'])
       .then(hl.tableize)
       .then((data) => {
-        console.log('return', JSON.stringify(data, null, 2))
+        this.data = data
       })
+  })
+
+  it('parses 1st account', function () {
+    expect(this.data[0])
+      .toEqual({ account: 'Assets', balance: '$250' })
+  })
+
+  it('parses 2nd account', function () {
+    expect(this.data[1])
+      .toEqual({ account: 'Assets:Checking', balance: '$150' })
   })
 })
